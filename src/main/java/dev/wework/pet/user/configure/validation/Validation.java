ca2e1fa3cda7186450bba1ea611b19bea39e5d56
Korcp <pm6567@naver.com>
2025-10-22 15:58:00 +0900

    feat : 비밀번호 유효성검사 및 이름 조건 추가

---
+++ src/main/java/dev/wework/pet/user/configure/validation/Validation.java
@@ -0,0 +1,14 @@
+package dev.wework.pet.user.configure.validation;
+
+public class Validation {
+
+    public static boolean isValidPassword(String password) {
+        String passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$";
+        return password.matches(passwordRegex);
+    }
+
+    public static boolean isValidNickname(String nickname) {
+        String nicknameRegex = "^[가-힣a-zA-Z0-9]{3,8}$";
+        return nickname.matches(nicknameRegex);
+    }
+}
