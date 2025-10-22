3bf29f638331267e7aaa8d891ce2ef8a000499e0
Korcp <pm6567@naver.com>
2025-10-22 15:55:27 +0900

    refactor : String message 매개변수 삭제

--- src/main/java/dev/wework/pet/user/signup/exception/NotMatchClassficationException.java
+++ src/main/java/dev/wework/pet/user/signup/exception/NotMatchClassficationException.java
@@ -5,5 +5,5 @@ import lombok.Getter;
 @Getter
 public class NotMatchClassficationException extends RuntimeException {
 
-    public NotMatchClassficationException(String message) { super("유효하지 않는 유형입니다."); }
+    public NotMatchClassficationException() { super("유효하지 않는 유형입니다."); }
 }
