f461d6f9ee969bb462e15f232d2bdc32151c7b07
byeonkwon <cbk5126@gmail.com>
2025-10-24 15:42:28 +0900

    test : 심사원 마이페이지 정보 제공 테스트

--- src/test/java/dev/wework/pet/mypage/MypageTest.java
+++ src/test/java/dev/wework/pet/mypage/MypageTest.java
@@ -1,8 +1,16 @@
 package dev.wework.pet.mypage;
 
-import dev.wework.pet.mypage.dto.ReviewMypageDTO;
-import dev.wework.pet.mypage.repository.ReviewMypageRepository;
-import dev.wework.pet.mypage.service.ReviewMypageService;
+import dev.wework.pet.mypage.dto.Request.ReviewerMyPageRequest;
+import dev.wework.pet.mypage.dto.Response.ReviewerMyPageResponse;
+import dev.wework.pet.mypage.service.ReviewerMypageService;
+import dev.wework.pet.user.signup.dto.Classification;
+import dev.wework.pet.user.signup.dto.Reviewergrade;
+import dev.wework.pet.user.signup.entity.Grade;
+import dev.wework.pet.user.signup.entity.Reviewer;
+import dev.wework.pet.user.signup.entity.User;
+import dev.wework.pet.user.signup.repository.GradeRepository;
+import dev.wework.pet.user.signup.repository.ReviewerRepository;
+import dev.wework.pet.user.signup.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,17 +21,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class MypageTest {
 
     @Autowired
-    private ReviewMypageRepository reviewMypageRepository;
+    private GradeRepository gradeRepository;
     @Autowired
-    private ReviewMypageService reviewMypageService;
+    private UserRepository userRepository;
+    @Autowired
+    private ReviewerRepository reviewerRepository;
+    @Autowired
+    private ReviewerMypageService reviewerMypageService;
 
     @Test
     @Transactional
     @DisplayName("심사원 마이페이지 정보")
     public void reviewerMypageTest(){
 
-        ReviewMypageDTO reviewMypageDTO = new ReviewMypageDTO(1,1,1,1);
-        reviewMypageService.findReviewerInfo(reviewMypageDTO);
-        System.out.println("당신의 회원 등급은 : " + reviewMypageDTO.user_grade() + " 당신의 심사원 등급은 : " + reviewMypageDTO.reviewer_grade());
+        ReviewerMyPageRequest user = new ReviewerMyPageRequest(5);
+
+
+        ReviewerMyPageResponse response = reviewerMypageService.ReviewerMypageInfo(user);
+
+        String loginId = response.loginID();
+        String userName = response.name();
+        Reviewergrade reviewergrade = response.reviewerGrade();
+
+        System.out.println("회원님의 ID : " + loginId + " 회원의 이름은 : " + userName + " 회원의 등급은 : " + reviewergrade);
+
     }
 }
