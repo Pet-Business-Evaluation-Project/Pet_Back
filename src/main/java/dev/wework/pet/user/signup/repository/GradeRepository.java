3ffeec1b7c8136bddd60aa8d100c779bee869ac4
byeonkwon <cbk5126@gmail.com>
2025-10-24 15:44:58 +0900

    feat : reviewer_id로 심사원 등급 찾는 기능 추가

--- src/main/java/dev/wework/pet/user/signup/repository/GradeRepository.java
+++ src/main/java/dev/wework/pet/user/signup/repository/GradeRepository.java
@@ -1,7 +1,12 @@
 package dev.wework.pet.user.signup.repository;
 
 import dev.wework.pet.user.signup.entity.Grade;
+import dev.wework.pet.user.signup.entity.Reviewer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GradeRepository extends JpaRepository<Grade, Integer> {
+
+    Optional<Grade> findByReviewerReviewerId(int reviewer_id);
 }
