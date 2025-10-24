7aa1f6e8a1b5e5e31a50812d84459c628877ea71
byeonkwon <cbk5126@gmail.com>
2025-10-24 15:46:07 +0900

    feat : 요청 받을 때 user_id 값으로 user 정보 반환 기능 추가

--- src/main/java/dev/wework/pet/user/signup/repository/UserRepository.java
+++ src/main/java/dev/wework/pet/user/signup/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package dev.wework.pet.user.signup.repository;
 
+import dev.wework.pet.mypage.dto.Request.ReviewerMyPageRequest;
 import dev.wework.pet.user.signup.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByLoginIDIgnoreCase(String loginID);
+
+    Optional<User> findByUserId(int userId);
 }
