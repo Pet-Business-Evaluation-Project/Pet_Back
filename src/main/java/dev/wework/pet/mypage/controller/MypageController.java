1ddd8cbe656ccdfe5d2e28d37b9a221403328f98
byeonkwon <cbk5126@gmail.com>
2025-10-25 21:38:46 +0900

    feat : 심사원 목록불러오는 기능 추가

--- src/main/java/dev/wework/pet/mypage/controller/MypageController.java
+++ src/main/java/dev/wework/pet/mypage/controller/MypageController.java
@@ -1,9 +1,12 @@
 package dev.wework.pet.mypage.controller;
 
 import dev.wework.pet.mypage.dto.Request.ReviewerInviteRequest;
+import dev.wework.pet.mypage.dto.Request.ReviewerListRequest;
 import dev.wework.pet.mypage.dto.Request.ReviewerMyPageRequest;
 import dev.wework.pet.mypage.dto.Response.ReviewerInviteResponse;
+import dev.wework.pet.mypage.dto.Response.ReviewerListResponse;
 import dev.wework.pet.mypage.dto.Response.ReviewerMyPageResponse;
+import dev.wework.pet.mypage.service.AdminMypageService;
 import dev.wework.pet.mypage.service.ReviewerMypageService;
 import dev.wework.pet.user.signup.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +18,16 @@ import java.util.List;
 @RequestMapping("/mypage")
 public class MypageController {
 
+
+    private final ReviewerMypageService reviewerMypageService;
+    private final AdminMypageService adminMypageService;
+
     @Autowired
-    private ReviewerMypageService reviewerMypageService;
+    public  MypageController(ReviewerMypageService reviewerMypageService, AdminMypageService adminMypageService) {
+        this.reviewerMypageService = reviewerMypageService;
+        this.adminMypageService = adminMypageService;
+    }
+
 
     @PostMapping("/reviewer")
     public ReviewerMyPageResponse ReviewerMyPage(@RequestBody ReviewerMyPageRequest request){
@@ -33,6 +44,14 @@ public class MypageController {
 
         return member;
 
+    }
+
+    @PostMapping("/admin")
+    public List<ReviewerListResponse> ReviewerList(@RequestBody ReviewerListRequest request){
+
+        List<ReviewerListResponse> reviewers = adminMypageService.getReviewerList(request);
+
+        return reviewers;
     }
 
 }
