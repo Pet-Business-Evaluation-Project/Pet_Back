6b0bd8c0705c5b445d8c1cb3ae0f36328671de64
byeonkwon <cbk5126@gmail.com>
2025-10-26 00:08:51 +0900

    feat : 심사원 등급 변경 로직 추가

--- src/main/java/dev/wework/pet/mypage/service/AdminMypageService.java
+++ src/main/java/dev/wework/pet/mypage/service/AdminMypageService.java
@@ -1,5 +1,7 @@
 package dev.wework.pet.mypage.service;
 
+import dev.wework.pet.exception.NotExistReviewerIdException;
+import dev.wework.pet.mypage.dto.Request.GradeUpdateRequest;
 import dev.wework.pet.mypage.dto.Request.ReviewerListRequest;
 import dev.wework.pet.mypage.dto.Response.ReviewerListResponse;
 import dev.wework.pet.user.signup.dto.Classification;
@@ -8,9 +10,11 @@ import dev.wework.pet.user.signup.entity.Reviewer;
 import dev.wework.pet.user.signup.repository.GradeRepository;
 import dev.wework.pet.user.signup.repository.ReviewerRepository;
 import dev.wework.pet.user.signup.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -56,6 +60,22 @@ public class AdminMypageService {
                 .distinct()
                 .collect(Collectors.toList());
 
+    }
+
+    public List<String> updateReviewerGrade(GradeUpdateRequest request) {
+
+        List<String> result = new ArrayList<>();
+
+        for(GradeUpdateRequest.GradeUpdateItem item : request.updates()) {
+            Grade grade = gradeRepository.findByReviewerReviewerId(item.reviewer_id())
+                    .orElseThrow(() -> new NotExistReviewerIdException());
+
+            grade.setReviewerGrade(item.reviewergrade());
+            gradeRepository.save(grade);
+            System.out.println("수정된 grade = " + grade.getReviewerGrade());
+            result.add("Updated reviewer grade " + grade.getReviewerGrade());
+        }
+        return result;
     }
 
 }
