7984bfa533818904f746bb787c5afa99374a43dc
byeonkwon <cbk5126@gmail.com>
2025-11-04 21:57:14 +0900

    feat : 컨트롤러 비밀번호 변경 요청 추가

--- src/main/java/dev/wework/pet/user/findpassword/controller/FindPasswordController.java
+++ src/main/java/dev/wework/pet/user/findpassword/controller/FindPasswordController.java
@@ -1,6 +1,8 @@
 package dev.wework.pet.user.findpassword.controller;
 
+import dev.wework.pet.user.findpassword.dto.Request.PasswordChangeRequest;
 import dev.wework.pet.user.findpassword.dto.Request.UserCheckRequest;
+import dev.wework.pet.user.findpassword.dto.Response.PasswordChangeResponse;
 import dev.wework.pet.user.findpassword.dto.Response.UserCheckResponse;
 import dev.wework.pet.user.findpassword.service.FindPasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,14 @@ public class FindPasswordController {
     public UserCheckResponse UserCheck(@RequestBody UserCheckRequest request){
 
         UserCheckResponse response = findPasswordService.ExistUserCheck(request);
+
+        return response;
+    }
+
+    @PostMapping("/changepassword")
+    public PasswordChangeResponse PasswordChange(@RequestBody PasswordChangeRequest request){
+
+        PasswordChangeResponse response = findPasswordService.ChangePassword(request);
 
         return response;
     }
