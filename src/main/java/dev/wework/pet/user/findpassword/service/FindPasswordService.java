49787ab326c324c8eb34ee99a5649e9c50e0fb01
byeonkwon <cbk5126@gmail.com>
2025-11-04 21:58:48 +0900

    feat : 서비스 비밀번호 변경 로직 추가

--- src/main/java/dev/wework/pet/user/findpassword/service/FindPasswordService.java
+++ src/main/java/dev/wework/pet/user/findpassword/service/FindPasswordService.java
@@ -1,6 +1,10 @@
 package dev.wework.pet.user.findpassword.service;
 
+import dev.wework.pet.exception.NotExistUserIdException;
+import dev.wework.pet.user.configure.encode.PasswordEncoderBCrypt;
+import dev.wework.pet.user.findpassword.dto.Request.PasswordChangeRequest;
 import dev.wework.pet.user.findpassword.dto.Request.UserCheckRequest;
+import dev.wework.pet.user.findpassword.dto.Response.PasswordChangeResponse;
 import dev.wework.pet.user.findpassword.dto.Response.UserCheckResponse;
 import dev.wework.pet.user.findpassword.exception.NotExistLoginIDException;
 import dev.wework.pet.user.findpassword.exception.NotExistSsnException;
@@ -17,9 +21,12 @@ public class FindPasswordService {
     UserRepository userRepository;
     ReviewerRepository  reviewerRepository;
 
-    public FindPasswordService(UserRepository userRepository, ReviewerRepository reviewerRepository){
+    private final PasswordEncoderBCrypt passwordEncoderBCrypt;
+
+    public FindPasswordService(UserRepository userRepository, ReviewerRepository reviewerRepository,  PasswordEncoderBCrypt passwordEncoderBCrypt) {
         this.userRepository = userRepository;
         this.reviewerRepository = reviewerRepository;
+        this.passwordEncoderBCrypt = passwordEncoderBCrypt;
     }
 
 
@@ -38,6 +45,17 @@ public class FindPasswordService {
         }
 
         return new UserCheckResponse(user.getUserId());
+    }
+
+    public PasswordChangeResponse ChangePassword(PasswordChangeRequest request){
+
+        User user = userRepository.findByUserId(request.userId()).orElseThrow(() -> new NotExistUserIdException());
+
+        String encodedPassword = passwordEncoderBCrypt.encode(request.password());
+        user.setPassword(encodedPassword);
+        userRepository.save(user);
+
+        return new PasswordChangeResponse(encodedPassword);
     }
 
 
