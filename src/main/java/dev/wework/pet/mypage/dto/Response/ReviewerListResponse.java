21b36e414479eb35f34ac4c37014638a2d81a1db
byeonkwon <cbk5126@gmail.com>
2025-11-17 22:17:09 +0900

    feat : 관리자 마이페이지 심사원 불러오기에 필요한 정보들(주소, 계좌, 은행명) 추가

--- src/main/java/dev/wework/pet/mypage/dto/Response/ReviewerListResponse.java
+++ src/main/java/dev/wework/pet/mypage/dto/Response/ReviewerListResponse.java
@@ -1,4 +1,4 @@
 package dev.wework.pet.mypage.dto.Response;
 
-public record ReviewerListResponse (int user_id, int reviewer_id, String name, String loginID, String phnum, String ssn , String reviewerGrade, String referralID , String referralGrade){
+public record ReviewerListResponse (int user_id, int reviewer_id, String name, String loginID, String phnum, String ssn, String address, String bankname, String account, String reviewerGrade, String referralID, String referralGrade){
 }
