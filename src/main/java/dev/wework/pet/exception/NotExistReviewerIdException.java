f5ee011a30f67f73b75b2b3d9694583b7dc297cd
byeonkwon <cbk5126@gmail.com>
2025-11-19 14:14:54 +0900

    feat : 심사원 아이디 예외처리 클래스 예외 추가

--- src/main/java/dev/wework/pet/exception/NotExistReviewerIdException.java
+++ src/main/java/dev/wework/pet/exception/NotExistReviewerIdException.java
@@ -1,5 +1,13 @@
+// NotExistReviewerIdException.java
 package dev.wework.pet.exception;
 
-public class NotExistReviewerIdException extends NullPointerException {
-    public NotExistReviewerIdException() {super("심사원 마이페이지를 볼 권한이 없습니다.");}
+public class NotExistReviewerIdException extends RuntimeException {  // NullPointerException 말고 RuntimeException으로!
+
+    public NotExistReviewerIdException() {
+        super("심사원 마이페이지에 접근할 권한이 없습니다.");
+    }
+
+    public NotExistReviewerIdException(String message) {
+        super(message);
+    }
 }
