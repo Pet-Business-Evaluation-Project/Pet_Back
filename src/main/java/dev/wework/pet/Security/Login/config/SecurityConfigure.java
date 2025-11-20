eff668fb5fab559199868819851554d1a2ab2e0b
Korcp <pm6567@naver.com>
2025-11-21 01:01:29 +0900

    refacotr : 롤백 ㅇ권한

--- src/main/java/dev/wework/pet/Security/Login/config/SecurityConfigure.java
+++ src/main/java/dev/wework/pet/Security/Login/config/SecurityConfigure.java
@@ -70,6 +70,7 @@ public class SecurityConfigure {
                                 "/community/**",
                                 "/expertise/**"
                         ).permitAll()
+                        .requestMatchers("/signstart/**").hasAnyRole("ADMIN", "REVIEWER","COMPANY")
                         .requestMatchers("/mypage/**").authenticated()
                         .requestMatchers("/api/admin/**","/admin/approval/**").hasRole("ADMIN")
                         .requestMatchers("/api/reviewer/**").hasRole("REVIEWER")
