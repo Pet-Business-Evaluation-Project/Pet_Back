4a487bd2095ae2290c50b2e31ea6474c91f4fe60
byeonkwon <cbk5126@gmail.com>
2025-11-25 12:02:40 +0900

    feat : 관리자 마이페이지 심사원 목록 DTO 전문분야 추가

--- src/main/java/dev/wework/pet/mypage/dto/Response/ReviewerListResponse.java
+++ src/main/java/dev/wework/pet/mypage/dto/Response/ReviewerListResponse.java
@@ -3,6 +3,6 @@ package dev.wework.pet.mypage.dto.Response;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record ReviewerListResponse (int user_id, int reviewer_id, String name, String loginID, String phnum, String ssn, String address, String bankname, String account, String reviewerGrade, String referralID, String referralGrade,
+public record ReviewerListResponse (int user_id, int reviewer_id, String name, String loginID, String phnum, String ssn, String address, String bankname, String account,String expertises ,String reviewerGrade, String referralID, String referralGrade,
                                     LocalDate created_at){
 }
